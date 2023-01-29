c43676c8bf4d8ac5c184ab64aadd296d365aa3a2
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-01-28 20:41:46 -0500

    add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,8 @@ module.exports = {
         "lemon": "#f5cb5c",
         "fire": "#f2b172",
         "aqua": '#42BB8A',
+        "light-aqua": "#87dbbc",
+        "dark-aqua": "#39b07e",
         "steel": "#DEE2E6",
         "charcoal": "#343A40",
         "black": "#212529",
@@ -26,8 +28,8 @@ module.exports = {
     backgroundImage: (theme) => ({
       "gradient-rainbow":
         "linear-gradient(81.66deg, #00B5EE 7.21%, #FF45A4 45.05%, #FFBA00 78.07%)",
-      "gradient-rainblue":
-        "linear-gradient(90deg, #24CBFF 14.53%, #FC59FF 69.36%, #FFBD0C 107.73%)",
+      "gradient-aqua":
+        "linear-gradient(90deg, #63caa2 14.53%, #42BB8A 69.36%, #39b07e 107.73%)",
     }),
     fontFamily: {
       dosis: ['Dosis', 'sans-serif'],
