19f301e8d4ec7e600336bda5bb4abf03f902a44d
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-02-14 16:51:56 -0500

    import theme context and add dynamic styles

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -1,35 +1,57 @@
-import linkedin from '../assets/linkedin.png';
-import twitter from '../assets/twitter.png'
-import {AiFillLinkedin} from 'react-icons/ai';
-import {AiOutlineTwitter} from 'react-icons/ai';
-import {AiOutlineGithub} from 'react-icons/ai';
+import {
+  AiFillLinkedin,
+  AiOutlineTwitter,
+  AiOutlineGithub,
+} from "react-icons/ai";
+import { ThemeContext } from "../context/ThemeContext";
+import { useContext } from "react";
 
-const SocialMediaIcons = () => {
+const SocialMediaIcons = ({ page }) => {
+  const { theme } = useContext(ThemeContext);
+
   return (
     <div className="flex justify-center md:justify-start my-10 gap-7">
       <a
-        className="hover:text-light-aqua transition duration-200"
+        className={`${
+          page === "landing"
+            ? theme === "dark"
+              ? "text-steel hover:text-aqua"
+              : "text-carbon hover:text-iron"
+            : "text-steel hover:text-light-aqua"
+        } transition duration-200`}
         href="https://www.linkedin.com/in/johnsguerrero/"
         target="_blank"
         rel="noreferrer"
       >
-        <AiFillLinkedin size={'35px'} />
+        <AiFillLinkedin size={"35px"} />
       </a>
       <a
-        className="hover:text-light-aqua transition duration-200"
+        className={`${
+          page === "landing"
+            ? theme === "dark"
+              ? "text-steel hover:text-aqua"
+              : "text-carbon hover:text-iron"
+            : "text-steel hover:text-light-aqua"
+        } hover:text-light-aqua transition duration-200`}
         href="https://twitter.com/SebaCodes"
         target="_blank"
         rel="noreferrer"
       >
-        <AiOutlineTwitter size={'35px'}/>
+        <AiOutlineTwitter size={"35px"} />
       </a>
       <a
-        className="hover:text-light-aqua transition duration-200"
+        className={`${
+          page === "landing"
+            ? theme === "dark"
+              ? "text-steel hover:text-aqua"
+              : "text-carbon hover:text-iron"
+            : "text-steel hover:text-light-aqua"
+        } hover:text-light-aqua transition duration-200`}
         href="https://github.com/JohnSebastian3"
         target="_blank"
         rel="noreferrer"
       >
-        <AiOutlineGithub size={'35px'}/>
+        <AiOutlineGithub size={"35px"} />
       </a>
     </div>
   );
