48d7abcb1a7b618fced04a52b9a74c2777ec0f3e
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-02-14 16:51:10 -0500

    add slate default color

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,17 +11,18 @@ module.exports = {
         gray: "#EDEDED",
         // "deep-blue": "#010026",
         // "dark-gray": "#757575",
-        "lemon": "#f5cb5c",
-        "fire": "#f2b172",
-        "aqua": '#42BB8A',
+        lemon: "#f5cb5c",
+        fire: "#f2b172",
+        aqua: "#42BB8A",
         "light-aqua": "#87dbbc",
         "dark-aqua": "#39b07e",
-        "steel": "#DEE2E6",
-        "charcoal": "#343A40",
-        "black": "#212529",
-        "carbon": "#495057",
+        steel: "#DEE2E6",
+        slate: "#EFF3F7",
+        charcoal: "#343A40",
+        black: "#212529",
+        carbon: "#495057",
         "dark-gray": "#6C757D",
-        "iron": "#ADB5BD",
+        iron: "#ADB5BD",
         "opaque-black": "rgba(0,0,0,0.35)",
       },
     },
@@ -32,8 +33,8 @@ module.exports = {
         "linear-gradient(90deg, #63caa2 14.53%, #42BB8A 69.36%, #39b07e 107.73%)",
     }),
     fontFamily: {
-      dosis: ['Dosis', 'sans-serif'],
-      sofiasans: ['Sofia Sans', 'sans-serif']
+      dosis: ["Dosis", "sans-serif"],
+      sofiasans: ["Sofia Sans", "sans-serif"],
     },
     content: {
       brush: "url('./assets/greenbrush.png')",
@@ -42,8 +43,8 @@ module.exports = {
       xs: "480px",
       sm: "768px",
       md: "1060px",
-      lg: "1480px"
-    }
+      lg: "1480px",
+    },
   },
   plugins: [],
 };
